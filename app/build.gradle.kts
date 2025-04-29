f1a678411423da2e615d7cdf71141108d9f27538
thirumalesh99 <thirumaleshlucky1999@gmail.com>
2025-04-29 19:43:47 +0530

    Fixes in dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,8 +66,6 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
-    implementation(libs.androidx.room.common.jvm)
-    implementation(libs.androidx.room.runtime.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -95,7 +93,7 @@ dependencies {
 
 
     implementation("androidx.biometric:biometric:1.4.0-alpha02")
-    implementation(kotlin("script-runtime"))
+
 
 
 }
